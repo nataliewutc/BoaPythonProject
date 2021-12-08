5fafe0a09a0dca85592cfdb545c184801224672c
nataliewutc <81494714+nataliewutc@users.noreply.github.com>
2021-12-08 18:26:24 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import requests
+import csv
 
 def random_manga():
     number = random.randint(1, 151) #generates random number from 1-150
@@ -7,13 +8,13 @@ def random_manga():
     request = requests.get(url)
     response_body = request.json()
     return {
-        'title': response_body["title"], 
+        'title': response_body["title"],
         'id': response_body["mal_id"],
         'rank': response_body["rank"],
         'popularity': response_body["popularity"]}
 
 def run():
-    # Ask players their names 
+    # Ask players their names
     name1 = input('Player 1, what is your name?')
     name2 = input('Player 2, what is your name?')
     #Randomly assign two characters for player 1
@@ -31,12 +32,30 @@ def run():
     print('{}, you were given {}'.format(name2, player2_character['title']))
     player2_choice = input('{}, which stat do you want to use? (id, rank, popularity) '.format(name2))
     player2_stat = player2_character[player2_choice]
-    #Compares the stats chosen by each player 
+    #Compares the stats chosen by each player
     if player1_stat > player2_stat:
         print('Player 1 Wins!')
     elif player1_stat < player2_stat:
         print('Player 2 Wins!')
     else:
         print('Draw!')
-        
+    score1 = 0
+    score2 = 0
+    if player1_stat > player2_stat:
+        score1=score1+1
+    elif player1_stat < player2_stat:
+        score2=score2+1
+    else:
+        score1=score1+0
+    field_names=['name','score']
+    data = [{'name':name1,'score':score1},{'name':name2, 'score':score2}]
+    with open ('scores.csv','w+') as csv_file:
+        spreadsheet =csv.DictWriter(csv_file, fieldnames=field_names)
+        spreadsheet.writeheader()
+        spreadsheet.writerows(data)
+    return
+
+
 run()
+
+
