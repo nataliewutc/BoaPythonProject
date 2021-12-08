fa7d2f2ce30a5fc958b4827b41d4d82674ec3750
nataliewutc <81494714+nataliewutc@users.noreply.github.com>
2021-12-08 01:19:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ def random_manga():
         'title': response_body["title"], 
         'id': response_body["mal_id"],
         'rank': response_body["rank"],
-        'popularity': response_body["popularity"]
-    }
+        'popularity': response_body["popularity"]}
 
 def run():
     my_character = random_manga()
-    print('You were given {}'.format(my_character['title']))
-    choice = input('Which stat do you want to use? id, rank, popularity')
+    my_character2 = random_manga()
+    print('You were given {} and {}'.format(my_character['title'], my_character2['title']))
+    char_choice = input('Which character do you want to choose?')
+    if char_choice == my_character['title']:
+        choice = input('Which stat do you want to use? id, rank, popularity')
+        my_stat = my_character[choice]
+    elif char_choice == my_character2['title']:
+        choice = input('Which stat do you want to use? id, rank, popularity')
+        my_stat = my_character2[choice]
     opponent_character = random_manga()
     print('The opponent chose {}'.format(opponent_character['title']))
-    my_stat = my_character[choice]
     opponent_stat = opponent_character[choice]
     if my_stat > opponent_stat:
         print('You Win!')
@@ -27,4 +32,5 @@ def run():
         print('You Lose!')
     else:
         print('Draw!')
+        
 run()
